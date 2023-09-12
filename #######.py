1c7c5937c1ede5c7c0d150e13793e978cf5716f8
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-12 15:33:09 +0200

    Update #######.py

--- #######.py
+++ #######.py
@@ -5,13 +5,16 @@ import cv2
 while (cv2.waitKey() !=27):
     image = cv2.imread('fail.png')
     cv2.imshow('windows ', image)
+
     image1 = cv2.imread('fail.png')
     cv2.imshow('Windows ', image1)
+
     image2 = cv2.imread('fail.png')
-    cv2.imshow('Windows ', image2)
+    cv2.imshow('vindows ', image2)
+
     image3 = cv2.imread('fail.png')
-    cv2.imshow('Windows ', image3)
+    cv2.imshow('Vindows ', image3)
 cv2.waitKey(0)  
   
 # Window shown waits for any key pressing event
-#cv2.destroyAllWindows()
+cv2.destroyAllWindows()
