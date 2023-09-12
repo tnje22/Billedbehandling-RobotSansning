d900fee3cae9e82c40b6794354064f3a7ac2a44c
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-12 16:03:51 +0200

    Update #######.py

--- #######.py
+++ #######.py
@@ -1,19 +1,19 @@
 # import opencv
+
 import cv2
-  
+x = 1
+y = 1
+
+cv2.namedWindow("windows")
 # Load the input image
 while (cv2.waitKey() !=27):
-    image = cv2.imread('fail.png')
-    cv2.imshow('windows ', image)
-    cv2.moveWindow("windows", 20,20)
-    image1 = cv2.imread('fail.png')
-    cv2.imshow('Windows ', image1)
-    cv2.moveWindow("Windows", 40,20)
-    image2 = cv2.imread('fail.png')
-    cv2.imshow('vindows ', image2)
-    cv2.moveWindow("vindows", 60,20)
-    image3 = cv2.imread('fail.png')
-    cv2.imshow('Vindows ', image3)
-    cv2.moveWindow("Vindows", 80,20)
+    cv2.waitKey(0)
+    cv2.moveWindow("windows", 1200,700)
+    cv2.waitKey(0)
+    cv2.moveWindow("windows", 200,200)
+    cv2.waitKey(0)
+    cv2.moveWindow("windows", 200,700)
+    cv2.waitKey(0)
+    cv2.moveWindow("windows", 1200,200)
 # Window shown waits for any key pressing event
 cv2.destroyAllWindows()
