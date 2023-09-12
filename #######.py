8c1ed5b405004ad06ecd2b43901cfce9f29dc017
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-12 15:15:49 +0200

    Update #######.py

--- #######.py
+++ #######.py
@@ -2,14 +2,16 @@
 import cv2
   
 # Load the input image
-image = cv2.imread('lion.jpg')
-cv2.imshow('Original', image)
-  
-# Use the cvtColor() function to grayscale the image
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  
-cv2.imshow('Grayscale', gray_image)
+while (cv2.waitKey() !=27):
+    image = cv2.imread('fail.png')
+    cv2.imshow('windows ', image)
+    image1 = cv2.imread('fail.png')
+    cv2.imshow('Windows ', image1)
+    image2 = cv2.imread('fail.png')
+    cv2.imshow('Windows ', image2)
+    image3 = cv2.imread('fail.png')
+    cv2.imshow('Windows ', image3)
 cv2.waitKey(0)  
   
 # Window shown waits for any key pressing event
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
