f04d41d09167f9285a1316fd7e2cc1e99ba07864
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-26 10:25:28 +0200

    Update mini v 0.1.py

--- mini projekt/mini v 0.1.py
+++ mini projekt/mini v 0.1.py
@@ -1,6 +1,6 @@
 # import opencv
 import cv2
 image = cv2.imread('King Domino dataset\Cropped and perspective corrected boards\1.jpg')
-cv2.imshow('Original', image)
+cv2.imshow('Plade', image)
 cv2.waitKey(0)  
 cv2.destroyAllWindows()
