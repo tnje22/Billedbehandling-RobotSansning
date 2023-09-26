fba327c90047610e32e4bfba46b6b371131b81d6
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-26 11:11:11 +0200

    new

--- mini projekt/mini v 0.1.py
+++ mini projekt/mini v 0.1.py
@@ -5,8 +5,6 @@ import numpy as np
 # Load the input image
 image = cv2.imread('mini projekt/1.jpg')
 cv2.imshow('Plade', image)
-
-print(image[0][0])
-
+print(img.shape)
 # Window shown waits for any key pressing event
 cv2.waitKey(0) 
