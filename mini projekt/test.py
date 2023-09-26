b4a3d18b66acbe1e18f88ed7a9e42715b8ee1eb7
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-26 11:43:39 +0200

    Update test.py

--- mini projekt/test.py
+++ mini projekt/test.py
@@ -6,14 +6,14 @@ img = cv2.imread('mini projekt/1.jpg')
 print(img.shape) # Print image shape
 cv2.imshow("original", img)
  
-# Cropping an image [x:x2, y:y2]
-cropped_image = img[100:200, 0:100]
- 
+# Cropping an image [y:y2, x:x2]
+square1 = img[0:100, 0:100]
 # Display cropped image
-cv2.imshow("cropped", cropped_image)
- 
-# Save the cropped image
-#cv2.imwrite("Cropped Image.jpg", cropped_image)
+cv2.imshow("square1", square1)
+ # Cropping an image [y:y2, x:x2]
+square2 = img[0:100, 100:200]
+# Display cropped image
+cv2.imshow("square2", square2)
  
 cv2.waitKey(0)
 cv2.destroyAllWindows()
