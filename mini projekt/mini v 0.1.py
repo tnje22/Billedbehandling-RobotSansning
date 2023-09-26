d42485033ce8863f1d05e96d6388913d98da990f
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-26 11:29:51 +0200

    new

--- mini projekt/mini v 0.1.py
+++ mini projekt/mini v 0.1.py
@@ -4,8 +4,11 @@ import numpy as np
 
 # Load the input image
 image = cv2.imread('mini projekt/1.jpg')
+
 cv2.imshow('Plade', image)
-print(image.shape)
-print(image)
+cv2.waitKey(0) 
+#print(image.shape)
+cropped_image = image[0:0, 100:100]
+cv2.imshow('croppedPlade', cropped_image)
 # Window shown waits for any key pressing event
 cv2.waitKey(0) 
