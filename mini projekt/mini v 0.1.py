017955895a55525c0b8dab110c6b730100ba24ea
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-26 12:14:08 +0200

    new

--- mini projekt/mini v 0.1.py
+++ mini projekt/mini v 0.1.py
@@ -1,14 +1,28 @@
-# import opencv
+# Import packages
 import cv2
 import numpy as np
+ 
+img = cv2.imread('mini projekt/1.jpg')
+#print(img.shape) # Print image shape
+cv2.imshow("original", img)
+ 
+# Cropping an image [y:y2, x:x2]
+square1 = img[0:100, 0:100]
+# taking the average color of the Cropend image
+acsquare1 = np.average(square1, axis=0)
+average_color1 = np.average(acsquare1, axis=0)
+# Display cropped image
+#cv2.imshow("square1", square1)
+# Printing  average color 
+print(average_color1)
 
-# Load the input image
-image = cv2.imread('mini projekt/1.jpg')
 
-cv2.imshow('Plade', image)
-cv2.waitKey(0) 
-#print(image.shape)
-cropped_image = image[0:0, 100:100]
-cv2.imshow('croppedPlade', cropped_image)
-# Window shown waits for any key pressing event
-cv2.waitKey(0) 
+square2 = img[0:100, 100:200]
+acsquare2 = np.average(square2, axis=0)
+average_color2 = np.average(acsquare2, axis=0)
+#cv2.imshow("square2", square2)
+print(average_color2)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- mini projekt/test.py
+++ mini projekt/test.py
@@ -1,19 +1,9 @@
-# Import packages
 import cv2
 import numpy as np
- 
+
 img = cv2.imread('mini projekt/1.jpg')
-print(img.shape) # Print image shape
-cv2.imshow("original", img)
- 
-# Cropping an image [y:y2, x:x2]
-square1 = img[0:100, 0:100]
-# Display cropped image
-cv2.imshow("square1", square1)
- # Cropping an image [y:y2, x:x2]
-square2 = img[0:100, 100:200]
-# Display cropped image
-cv2.imshow("square2", square2)
- 
+average_color_row = np.average(img, axis=0)
+average_color = np.average(average_color_row, axis=0)
+print(average_color)
+
 cv2.waitKey(0)
-cv2.destroyAllWindows()
