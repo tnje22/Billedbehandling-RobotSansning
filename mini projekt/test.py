a1d46f9111bfda2209e787121333f5e787ac6f95
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-09-26 11:37:48 +0200

    new

--- mini projekt/test.py
+++ mini projekt/test.py
@@ -1,14 +1,19 @@
-# import opencv
+# Import packages
 import cv2
 import numpy as np
-
-# Load the input image
-image = cv2.imread('mini projekt/1.jpg')
-
-cv2.imshow('Plade', image)
-cv2.waitKey(0) 
-#print(image.shape)
-cropped_image = image[0:0, 100:100]
-cv2.imshow('croppedPlade', cropped_image)
-# Window shown waits for any key pressing event
-cv2.waitKey(0) 
+ 
+img = cv2.imread('mini projekt/1.jpg')
+print(img.shape) # Print image shape
+cv2.imshow("original", img)
+ 
+# Cropping an image [x:x2, y:y2]
+cropped_image = img[100:200, 0:100]
+ 
+# Display cropped image
+cv2.imshow("cropped", cropped_image)
+ 
+# Save the cropped image
+cv2.imwrite("Cropped Image.jpg", cropped_image)
+ 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
